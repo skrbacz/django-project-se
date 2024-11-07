d62c180de8082aa310cb3d431ed0d641c2580ce1
Oliwia <oliwiaskrgit@gmail.com>
2024-11-07 12:01:35 +0100

    model product- fixed final
    # Conflicts:
    #	djangoProject/myapp/models.py

--- djangoProject/myapp/models.py
+++ djangoProject/myapp/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, unique=True, blank=False, null=False)
+    name = models.CharField(max_length=100)
     price = models.FloatField()
     available = models.BooleanField(default=False)
