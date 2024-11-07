1803128d83a8f1104c4e070ded2c872c11d8ccbc
Oliwia <oliwiaskrgit@gmail.com>
2024-11-07 12:28:19 +0100

    model product- added def __str__

--- djangoProject/myapp/models.py
+++ djangoProject/myapp/models.py
@@ -6,3 +6,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100, unique=True),
     price = models.FloatField()
     available = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
