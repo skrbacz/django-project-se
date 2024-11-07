2e8ec765ba52eea148e6d8c97c7df199313fb9a5
Oliwia <oliwiaskrgit@gmail.com>
2024-11-07 18:04:33 +0100

    model: order, product, customer- registrated

--- djangoProject/myapp/admin.py
+++ djangoProject/myapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Product, Customer, Order
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Order)
