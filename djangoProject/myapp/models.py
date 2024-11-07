cb8b3df7bfb9f72ab90a17ce94475d2556391bd7
Oliwia <oliwiaskrgit@gmail.com>
2024-11-07 11:37:43 +0100

    model product- fixed

--- djangoProject/myapp/models.py
+++ djangoProject/myapp/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True, blank=False, null=False)
     price = models.FloatField()
     available = models.BooleanField(default=False)
