0f54531efb22896363c9afba5bc60221a6ccc34c
Oliwia <oliwiaskrgit@gmail.com>
2024-11-07 15:38:06 +0100

    not sure what changed

--- djangoProject/myapp/models.py
+++ djangoProject/myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, unique=True),
+    name = models.CharField(max_length=100, unique=True)
     price = models.FloatField()
     available = models.BooleanField(default=False)
 
