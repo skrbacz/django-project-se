ca6bc7bdd481897e50bd6f496017f3001d75e23f
Oliwia <oliwiaskrgit@gmail.com>
2024-11-07 11:14:27 +0100

    product model- created

--- djangoProject/myapp/models.py
+++ djangoProject/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True),
+    name = models.CharField(max_length=100),
+    price = models.DecimalField(max_digits=10, decimal_places=2),
+    available = models.BooleanField(default=False)
