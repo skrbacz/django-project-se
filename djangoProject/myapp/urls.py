4b278f9f63096ce2e1d70bc65a5d4add5f954da5
Oliwia <oliwiaskrgit@gmail.com>
2024-11-28 16:49:39 +0100

    part_three- all tasks done

--- djangoProject/myapp/urls.py
+++ djangoProject/myapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, ProductListView, ProductDetailView, ProductCreateView
 urlpatterns = [
 path('hello/', hello_world, name='hello_world'),
+path('user/products/', ProductListView.as_view(), name='product_list'),
+path('user/products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+path('user/products/new/', ProductCreateView.as_view(), name='product_create'),
 ]
--- djangoProject/myapp/views.py
+++ djangoProject/myapp/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .forms import ProductForm
+from .models import Product
+
 
 def hello_world(request):
     return HttpResponse("Hello, World!")
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "product_list.html"
+    context_object_name = "products"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "product_detail.html"
+    context_object_name = "product"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "product_create.html"
+    success_url = '../../products/'
+
+    def form_valid(self, form):
+        return super().form_valid(form)
