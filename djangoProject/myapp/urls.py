8f9add0de0c4366fda676a944f86f9037d0189ba
Oliwia <oliwiaskrgit@gmail.com>
2024-11-28 16:21:52 +0100

    modified urls.py

--- djangoProject/myapp/urls.py
+++ djangoProject/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import hello_world, product_list, product_detail
 
 urlpatterns = [
-path('hello/', hello_world, name='hello_world'),
-path('api/products/', product_list, name='product_list'),
-path('api/products/<int:product_id>/',product_detail,name='product_detail'),
+    path('hello/', hello_world, name='hello_world'),
+    path('api/products/', product_list, name='product_list'),
+    path('api/products/<int:product_id>/', product_detail, name='product_detail'),
 ]
