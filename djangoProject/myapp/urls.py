87c1d5c6a39f72755d7cd9de1efe642205d68595
Oliwia <oliwiaskrgit@gmail.com>
2024-12-04 15:36:34 +0100

    feature_auth/permissions- patch http method problem

--- djangoProject/myapp/urls.py
+++ djangoProject/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import hello_world, ProductViewSet,OrderViewSet, CustomerViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
@@ -10,4 +11,6 @@ router.register(r'customers', CustomerViewSet, basename='customer')
 urlpatterns = [
     path('hello/', hello_world, name='hello_world'),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- djangoProject/myapp/views.py
+++ djangoProject/myapp/views.py
@@ -4,21 +4,27 @@ from rest_framework import viewsets
 from .models import Product, Order, Customer
 from .serializers import ProductSerializer, OrderSerializer, CustomerSerializer
 
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAdminOrReadOnly
+
 
 def hello_world(request):
     return HttpResponse("Hello, World!")
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
