581e1e9daea3eaa1fd2f569130dbb5f7e93ad98c
Oliwia <oliwiaskrgit@gmail.com>
2025-01-08 09:58:39 +0100

    list 9 part 1- done

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp',
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- djangoProject/myapp/urls.py
+++ djangoProject/myapp/urls.py
@@ -2,6 +2,21 @@ from django.urls import path, include
 from .views import hello_world, ProductViewSet,OrderViewSet, CustomerViewSet
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+openapi.Info(
+title="Software engineering lab",
+default_version="v1",
+description="API documentation for the lab",
+),
+public=True,
+permission_classes=(AllowAny,),
+authentication_classes=[],
+)
+
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
@@ -13,4 +28,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('swagger/', schema_view.with_ui('swagger',
+    cache_timeout=0), name='schema-swagger-ui'),
 ]
