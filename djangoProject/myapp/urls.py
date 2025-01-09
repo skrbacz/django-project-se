ad6b59ef73c329463cfc0c795cb7d4ec7fd318bf
Oliwia <oliwiaskrgit@gmail.com>
2025-01-09 19:56:29 +0100

    list9 part2 step9- trying to fix the api documentation page

--- djangoProject/myapp/urls.py
+++ djangoProject/myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import hello_world, ProductViewSet,OrderViewSet, CustomerViewSet
+from .views import hello_world, ProductViewSet, OrderViewSet, CustomerViewSet
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_yasg.views import get_schema_view
@@ -17,7 +17,6 @@ schema_view = get_schema_view(
     authentication_classes=[],
 )
 
-
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'orders', OrderViewSet, basename='order')
@@ -28,6 +27,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('swagger/', schema_view.with_ui('swagger',
-    cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
